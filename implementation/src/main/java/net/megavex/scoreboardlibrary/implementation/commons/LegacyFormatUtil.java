2ecdc8f97d556df8fccbcb4b54d4983f6c708810
vytskalt <vytskalt@protonmail.com>
2026-07-16 23:28:44 +0300

    fix setting player color on 1.8 via prefix (#119)

--- implementation/src/main/java/net/megavex/scoreboardlibrary/implementation/commons/LegacyFormatUtil.java
+++ implementation/src/main/java/net/megavex/scoreboardlibrary/implementation/commons/LegacyFormatUtil.java
@@ -1,6 +1,7 @@
 package net.megavex.scoreboardlibrary.implementation.commons;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyFormat;
@@ -14,6 +15,7 @@ import java.util.Map;
 import java.util.Objects;
 
 import static net.kyori.adventure.text.Component.empty;
+import static net.kyori.adventure.text.Component.space;
 import static net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer.legacySection;
 import static net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer.parseChar;
 
@@ -57,7 +59,24 @@ public final class LegacyFormatUtil {
       translated = component;
     }
 
-    return legacySection().serialize(translated);
+    String legacyFormat = legacySection().serialize(translated);
+
+    // Legacy format serializer ignores empty components, so it's impossible to set player color on 1.8 via team prefix
+    // Need to manually add the missing legacy format of the last component
+    Component lastChild = translated;
+    while (!lastChild.children().isEmpty()) {
+      lastChild = lastChild.children().get(0);
+    }
+
+    if (lastChild instanceof TextComponent) {
+      String content = ((TextComponent) lastChild).content();
+      if (content.isEmpty()) {
+        String ending = legacySection().serialize(lastChild.append(space()));
+        legacyFormat += ending.substring(0, ending.length() - 1);
+      }
+    }
+
+    return legacyFormat;
   }
 
   public static char getChar(@Nullable NamedTextColor color) {
---
+++ implementation/src/test/java/net/megavex/scoreboardlibrary/implementation/LegacyFormatUtilTest.java
@@ -0,0 +1,28 @@
+package net.megavex.scoreboardlibrary.implementation;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.megavex.scoreboardlibrary.implementation.commons.LegacyFormatUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static net.kyori.adventure.text.Component.text;
+
+class LegacyFormatUtilTest {
+  @Test
+  void serializeTest() {
+    assertSerialization(text(""), "");
+    assertSerialization(text(" ", NamedTextColor.RED), "§c ");
+    assertSerialization(text("", NamedTextColor.RED), "§c");
+
+    assertSerialization(text("Text", NamedTextColor.AQUA, TextDecoration.BOLD), "§b§lText");
+    Component nested = text("Text ", NamedTextColor.AQUA, TextDecoration.BOLD).append(text("", NamedTextColor.AQUA).append(text("", NamedTextColor.RED)));
+    assertSerialization(nested, "§b§lText §c");
+  }
+
+  private void assertSerialization(Component component, String expected) {
+    String legacy = LegacyFormatUtil.serialize(component, null);
+    Assertions.assertEquals(expected, legacy);
+  }
+}
